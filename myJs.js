02d92c5ab92badb1e5233644c2d744a7236bef94
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-06-23 12:48:22 +0300

    feat: finish Version 12 (Fri, Jun 23, 2023 12:48:22 PM)

--- myJs.js
+++ myJs.js
@@ -1,39 +1,40 @@
 let items = [];
 
-const list = document.querySelector('.list');
-const addPizzaForm = document.querySelector('.add-pizza-form');
+const list = document.querySelector(".list");
+const addPizzaForm = document.querySelector(".add-pizza-form");
 
 function handleSubmit(e) {
-    e.preventDefault();
-    const title = e.currentTarget.title.value;
-    const price = e.currentTarget.price.value;
-    const image = e.currentTarget.image.value;
-    const description = e.currentTarget.description.value
+  e.preventDefault();
+  const title = e.currentTarget.title.value;
+  const price = e.currentTarget.price.value;
+  const image = e.currentTarget.image.value;
+  const description = e.currentTarget.description.value;
 
-    if (!title || !price || !image || !description) {
-        return
-    };
+  if (!title || !price || !image || !description) {
+    return;
+  }
 
-    const item = {
-        title,
-        price,
-        image,
-        description,
-        selected: false,
-        id: Date.now()
-    };
-    
-    items.push(item);
-    e.currentTarget.reset();
-    displayItems();
+  const item = {
+    title,
+    price,
+    image,
+    description,
+    selected: false,
+    id: Date.now(),
+  };
+
+  items.push(item);
+  e.currentTarget.reset();
+
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 };
 
 const displayItems = () => {
-    const html = items.map(item => {
-        return (
-            `
+  const html = items
+    .map((item) => {
+      return `
                 <li class="pizza-item">
-                    <button class="delete">&times;</button>
+                    <button value="${item.id}"class="delete">&times;</button>
                     <div class="pizza-item-image">
                         <img src="${item.image}" alt="${item.title}">
                     </div>
@@ -48,98 +49,103 @@ const displayItems = () => {
                         <span class="pizza-items-details-bottom">${item.description}</span>
                     </div>
                 </li>
-            `
-        )
-    }).join('');
-    list.innerHTML = html;
+            `;
+    })
+    .join("");
+
+  list.innerHTML = html;
 };
-
-
-addPizzaForm.addEventListener('submit', handleSubmit);
-
 
 const editPizza = (pizzaId, newPizzaName) => {
+  const itemIndex = items.findIndex((res) => res.id === pizzaId);
+  items[itemIndex].title = newPizzaName;
 
-    const itemIndex = items.findIndex(res => res.id === pizzaId);
-    items[itemIndex].title = newPizzaName;
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
 
-    displayItems();
-
-    // items[pizzaIndex].title = newPizzaName;
-}
-
-const removePizza = (id) => {
-    items = items.filter(res => res.id !== id); 
-    
-    displayItems();
-
-
-    // items.splice(pizzaIndex, 1);
-}
-
-
-const markAsSelected = (id) => {
-
-    const itemIndex = items.findIndex(res => res.id === id);
-    const pizzaItem = items[itemIndex];
-    pizzaItem.selected = !pizzaItem.selected;
-    
-    displayItems()
-
-
-    // if (pizzaItem.selected) {
-    //     pizzaItem.selected = false;
-    // } else {
-    //     pizzaItem.selected = true
-    // }
-
-    // pizzaItem.selected === true ? pizzaItem.selected = false : pizzaItem.selected = true;
-} 
-
-
-const selectPizzas = () => {
-    let checkedPizza = 0;
-    let itemsChecked = [];
-
-    items.forEach(pizza => {
-        if (pizza.selected === true) {
-            checkedPizza += 1
-        };
-    });
-
-    if (checkedPizza === items.length) {
-        itemsChecked = items.map(pizza => {
-            return {
-                ...pizza, 
-                selected: false
-            }
-        });
-    } else {
-        itemsChecked = items.map(pizza => {
-            return {
-                ...pizza, 
-                selected: true
-            }
-        });
-    }
-
-    items = itemsChecked;
-
-    displayItems();
+  // items[pizzaIndex].title = newPizzaName;
 };
 
+const removePizza = (id) => {
+  items = items.filter((res) => res.id !== id);
+
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+
+  // items.splice(pizzaIndex, 1);
+};
+
+const markAsSelected = (id) => {
+  const itemIndex = items.findIndex((res) => res.id === id);
+  const pizzaItem = items[itemIndex];
+  pizzaItem.selected = !pizzaItem.selected;
+
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+
+  // if (pizzaItem.selected) {
+  //     pizzaItem.selected = false;
+  // } else {
+  //     pizzaItem.selected = true
+  // }
+
+  // pizzaItem.selected === true ? pizzaItem.selected = false : pizzaItem.selected = true;
+};
+
+const selectPizzas = () => {
+  let checkedPizza = 0;
+  let itemsChecked = [];
+
+  items.forEach((pizza) => {
+    if (pizza.selected === true) {
+      checkedPizza += 1;
+    }
+  });
+
+  if (checkedPizza === items.length) {
+    itemsChecked = items.map((pizza) => {
+      return {
+        ...pizza,
+        selected: false,
+      };
+    });
+  } else {
+    itemsChecked = items.map((pizza) => {
+      return {
+        ...pizza,
+        selected: true,
+      };
+    });
+  }
+
+  items = itemsChecked;
+
+  list.dispatchEvent(new CustomEvent("itemsUpdated"));
+};
+
+function saveToLocalStorage() {
+  console.log("LS save");
+  localStorage.setItem("items", JSON.stringify(items));
+};
+
+function getFromLocalStorage() {
+  const localStorageItems = JSON.parse(localStorage.getItem("items"));
+
+  if (localStorageItems && localStorageItems.length) {
+    items.push(...localStorageItems);
+    list.dispatchEvent(new CustomEvent("itemsUpdated"));
+  }
+};
+
+addPizzaForm.addEventListener("submit", handleSubmit);
+list.addEventListener("itemsUpdated", displayItems);
+list.addEventListener("itemsUpdated", saveToLocalStorage);
+// Делегирование событий. Слушаем событие на родительском UL 
+// Делегируем событие элементу button
+list.addEventListener('click', function(e){
+    if (e.target.matches('button')) {
+        removePizza(parseInt(e.target.value));
+    };
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
+getFromLocalStorage();
 
 
